41911e0bcbc35222089826ddd9b3f9f35ee3c7b2
Divya Rathore <rathoredivya150@gmail.com>
2026-05-23 18:40:32 +0530

    singeltonPattern

---
+++ SingletonPattern/Logger.java
@@ -0,0 +1,23 @@
+public class Loggger {
+    private static volatile Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            // here we are writing Logger.class becuase it is static function and don't have object => can't use this, so internally java create class object which have everything of object
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance
+    }
+    public void log(string message)
+    {
+        System.out.println("Log: "+message);
+    }
+
+}
